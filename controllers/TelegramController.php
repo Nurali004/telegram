740f1ab4d476e60e6e998e222c0ed6a189f896fc
M A C W I N <mavzurovnurali@gmail.com>
2025-08-08 00:16:50 +0500

    sendMessage

--- controllers/TelegramController.php
+++ controllers/TelegramController.php
@@ -6,10 +6,15 @@ use vendor\frame\Controller;
 
 class TelegramController extends Controller
 {
-    public function bot(){
 
+    public $url;
+
+    public function __construct(){
         $token = '8297930277:AAEeX9D0hmwxJdlDu7wtVXQ0dpHGzqrbCAw';
-        $apiUrl = 'https://api.telegram.org/bot'.$token.'/';
+        $this->url = 'https://api.telegram.org/bot'.$token.'/';
+
+    }
+    public function bot(){
 
         $content = file_get_contents('php://input');
         $update = json_decode($content, true);
@@ -17,29 +22,38 @@ class TelegramController extends Controller
         $chat_id = $update['message']['chat']['id'] ?? null;
         $text = $update['message']['text'] ?? '';
 
+
+    }
+
+    public function sendMessage($chat_id, $text){
+        $data = [
+            'chat_id' => $chat_id,
+            'text' => $text,
+            'parse_mode' => 'HTML'
+        ];
+
         if($text == 'Qalin' && $chat_id){
             $reply = "<b>". $text. "</b>";
-            file_get_contents("{$apiUrl}sendMessage?chat_id=$chat_id&parse_mode=HTML&text=" . urlencode($reply));
+            file_get_contents("{$this->url}sendMessage?". http_build_query($data));
         }elseif($text == 'Yotiq' && $chat_id){
             $reply = "<i>". $text. "</i>";
-            file_get_contents("{$apiUrl}sendMessage?chat_id=$chat_id&parse_mode=HTML&text=" . urlencode($reply));
+            file_get_contents("{$this->url}sendMessage?". http_build_query($data));
         }elseif($text == 'Pastgi chiziq' && $chat_id){
             $reply = "<u>". $text. "</u>";
-            file_get_contents("{$apiUrl}sendMessage?chat_id=$chat_id&parse_mode=HTML&text=" . urlencode($reply));
+            file_get_contents("{$this->url}sendMessage?". http_build_query($data));
         }elseif($text == 'Yotiq va Qalin' && $chat_id){
             $reply = "<b>".'<i>'. $text. '</i>'."</b>";
-            file_get_contents("{$apiUrl}sendMessage?chat_id=$chat_id&parse_mode=HTML&text=" . urlencode($reply));
+            file_get_contents("{$this->url}sendMessage?". http_build_query($data));
         }elseif($text == 'Yashirin' && $chat_id){
             $reply = $text.'<span class="tg-spoiler">Yashirin</span>';
-            file_get_contents("{$apiUrl}sendMessage?chat_id=$chat_id&parse_mode=HTML&text=" . urlencode($reply));
+            file_get_contents("{$this->url}sendMessage?". http_build_query($data));
         }elseif($text == 'Havola' && $chat_id){
             $reply = $text.'<a href="https://1.nugaev.uz">Bizning sayt</a>';
-            file_get_contents("{$apiUrl}sendMessage?chat_id=$chat_id&parse_mode=HTML&text=" . urlencode($reply));
+            file_get_contents("{$this->url}sendMessage?". http_build_query($data));
         }elseif($text == 'User' && $chat_id){
             $reply = '<a href="tg://user?id=6989752538">Nurali Mavzurov</a>';
-            file_get_contents("{$apiUrl}sendMessage?chat_id=$chat_id&parse_mode=HTML&text=" . urlencode($reply));
+            file_get_contents("{$this->url}sendMessage?". http_build_query($data));
         }
-
 
 
     }
