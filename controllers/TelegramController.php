52afc845a5a072cc596a5c5c4aa9319c98b9094f
M A C W I N <mavzurovnurali@gmail.com>
2025-08-08 00:47:29 +0500

    yangi yozuvlar botda

--- controllers/TelegramController.php
+++ controllers/TelegramController.php
@@ -43,6 +43,9 @@ class TelegramController extends Controller
         }elseif($text == 'User' && $chat_id){
             $reply = '<a href="tg://user?id=6989752538">Nurali Mavzurov</a>';
             $this->sendMessage($chat_id, $reply);
+        }elseif($text == 'Photo' && $chat_id){
+            $reply = 'Bu PHP Photo :';
+            $this->sendPhoto($chat_id, $reply, 'https://images.unsplash.com/photo-1599507593499-a3f7d7d97667?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGhwfGVufDB8fDB8fHww');
         }
 
 
@@ -58,6 +61,18 @@ class TelegramController extends Controller
         ];
 
         file_get_contents("{$this->url}sendMessage?". http_build_query($data));
+
+    }
+
+    public function sendPhoto($chat_id, $text, $photo){
+        $data = [
+            'chat_id' => $chat_id,
+            'caption' => $text,
+            'photo'=> $photo,
+            'parse_mode' => 'HTML'
+        ];
+
+        file_get_contents("{$this->url}sendPhoto?". http_build_query($data));
 
     }
 }
