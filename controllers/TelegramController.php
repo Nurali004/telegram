baaff18181d0fe52fe8f922d9ed697487f78cea9
M A C W I N <mavzurovnurali@gmail.com>
2025-08-07 23:31:26 +0500

    123

--- controllers/TelegramController.php
+++ controllers/TelegramController.php
@@ -18,8 +18,8 @@ class TelegramController extends Controller
         $text = $update['message']['text'] ?? '';
 
         if ($chat_id && $text) {
-            $reply = "Siz yozdingiz: " . $text;
-            file_get_contents("{$apiUrl}sendMessage?chat_id=$chat_id&text=" . urlencode($reply));
+            $reply = "*Siz yozdingiz: *" . $text;
+            file_get_contents("{$apiUrl}sendMessage?chat_id=$chat_id&parse_mode=MarkdownV2&text=" . urlencode($reply));
 
         }
 
