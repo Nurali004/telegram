508f52b7fa4209aaa11d353103462605d45e520f
Nugaev <sardor0418@mail.ru>
2025-08-13 10:30:23 +0500

    456

--- autoload.php
+++ autoload.php
@@ -1,5 +1,18 @@
 <?php
 
+//spl_autoload_register(function ($class) {
+//    // namespace'dagi "\" larni "/" ga aylantiramiz
+//    $classPath = str_replace("\\", "/", $class);
+//    $file = __DIR__ . '/' . $classPath . '.php';
+//
+//    if (file_exists($file)) {
+//        require_once $file;
+//    } else {
+//        throw new Exception("Class file not found: $file");
+//    }
+//});
+
+
 
 spl_autoload_register(function ($class) {
     $class = str_replace("\\", "/", $class);
